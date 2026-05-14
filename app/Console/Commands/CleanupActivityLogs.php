d9738d5c3c0f66de7f0ae01f943fbe998bfa210f
rainishmistry <mistryrainish7@gmail.com>
2026-05-14 21:01:59 +0530

    Clear Activity logs data from specific period of time

---
+++ app/Console/Commands/CleanupActivityLogs.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+// use Illuminate\Console\Attributes\Description;
+// use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use App\Models\ActivityLog;
+use Carbon\Carbon;
+use App\Helpers\ActivityLogger;
+
+// #[Signature('app:cleanup-activity-logs')]
+// #[Description('Command description')]
+class CleanupActivityLogs extends Command
+{
+
+    // Command name
+    protected $signature = 'logs:cleanup';
+
+    // Command description
+    protected $description = 'Delete old activity logs from database';
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $days = 1; // delete logs older than 3 days
+
+        $deleted = ActivityLog::where('created_at', '<', Carbon::now()->subDays($days))->forceDelete();
+
+        $this->info("Deleted {$deleted} activity logs older than {$days} days.");
+
+        ActivityLogger::log(
+            'cleanup_activity_logs',
+            "Deleted {$deleted} activity logs older than {$days} days."
+        );
+
+        return Command::SUCCESS;
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('logs:cleanup')->everyMinute()->appendOutputTo(storage_path('logs/cleanup_activity_logs.log'));
+
+
+// Run the schedule manually for local development >>>> php artisan schedule:run
